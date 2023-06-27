a458ffb49fa9ded24ad08b35cffb49aefcd57dd3
Wojtek Rak <wojtekrakdev@gmail.com>
2023-06-27 22:40:21 +0100

    added oprint to the project

--- utils.lua
+++ utils.lua
@@ -36,3 +36,14 @@ function print_mesh_info()
    print("BFC:"..(backface_culling and 'ON' or 'OFF').."(⬆️+🅾️)", 0, 117)
    print("AUTO-ROTATE:"..(auto_rotate and 'ON' or 'OFF').."(⬇️+🅾️)", 0, 123)
 end
+
+function oprint(t,x,y,c1,c2)
+	for i=0,2 do
+	 for j=0,2 do
+	  if not(i==1 and j==1) then
+	   print(t,x+i,y+j,c1)
+	  end
+	 end
+	end
+	print(t,x+1,y+1,c2)
+end
